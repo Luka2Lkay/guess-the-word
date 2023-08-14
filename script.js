1c0d1782345beaf4a5b704d440db39fdf7c4ec5f
Luka <lukhanyo95@gmail.com>
2023-08-14 16:52:10 +0200

    add localStorage

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const newWordButton = document.getElementById("new-word");
 const submitButton = document.getElementById("submit-button");
 const input = document.getElementById("correct-word");
 const message = document.getElementById("message");
+const showScore = document.getElementById("score");
 
 const words = [
   "shop",
@@ -17,9 +18,9 @@ const words = [
   "food",
 ];
 
-window.addEventListener("DOMContentLoaded", () => {
-  displayedWord.textContent = generateRandomLetters();
-});
+let score = localStorage.getItem("score") || 0 
+
+
 
 const generateRandomLetters = () => {
   const randomWords = words.sort(() => Math.random() - 0.5);
@@ -30,36 +31,45 @@ const generateRandomLetters = () => {
 
   if (!randomWords.includes(randomLetters)) {
     return randomLetters;
+  } else {
+    return generateRandomLetters();
   }
 };
 
+window.addEventListener("DOMContentLoaded", () => {
+  displayedWord.textContent = generateRandomLetters();
+  showScore.textContent = `Score: ${score}`;
+});
+
 const show = () => {
   submitButton.disabled = false;
-  displayedWord.textContent = '';
-  message.textContent = '';
+  displayedWord.textContent = "";
+  message.textContent = "";
   displayedWord.textContent = generateRandomLetters();
 };
 
 const answer = (e) => {
   const randomLettersLength = displayedWord.textContent.length;
 
-  if (input.value.length === randomLettersLength) {
-    for (let i = 0; i < randomLettersLength; i++) {
-      if (
-        displayedWord.textContent.search(input.value[i]) !== -1 &&
-        words.includes(input.value)
-      ) {
-        message.textContent = "Correct!";
-        message.style.color = "green";
-        e.target.disabled = true;
-      } else {
-        message.textContent = "Inorrect!";
-        message.style.color = "red";
-      }
-    }
+  const sortInputValue = input.value.split("").sort().join("");
+  const sortDisplayWord = displayedWord.textContent.split("").sort().join("");
+
+  if (
+    input.value.length === randomLettersLength &&
+    sortInputValue === sortDisplayWord &&
+    words.includes(input.value)
+  ) {
+    score++;
+    localStorage.setItem("score", score);
+    message.style.color = "green";
+    e.target.disabled = true;
+    showScore.textContent = `Score: ${score}`;
   } else {
-    message.textContent = "Enter equal number of letters";
+    score = score - 5;
+    e.target.disabled = true;
+    message.textContent = "Incorrect!";
     message.style.color = "red";
+    showScore.textContent = `Score: ${score}`;
   }
 };
 
